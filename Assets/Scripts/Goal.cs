896cdda25e6f56e5cc86fac0e571378a96e52f7f
Aaron <aaron.wedral@gmail.com>
2023-05-20 15:29:20 +0200

    Move goal screen shake to new goal events

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -7,13 +7,13 @@ using UnityEngine.Events;
 public class Goal : MonoBehaviour
 {
     
-    private ScreenShake screenShake;
+    
     public UnityEvent goal;
     public 
     // Start is called before the first frame update
     void Start()
     {
-        screenShake = Camera.main.GetComponent<ScreenShake>();
+        
     }
 
     // Update is called once per frame
@@ -28,11 +28,6 @@ public class Goal : MonoBehaviour
         if (other.gameObject.CompareTag("Ball"))
         {
             goal.Invoke();
-        }
-        // Shake screen on collision with player
-        if (screenShake != null & other.gameObject.CompareTag("Ball"))
-        {
-            screenShake.Shake(5f);
         }
     }
 }
